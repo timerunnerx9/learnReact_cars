85dbe74405eaba127fc7b84ed976897b6351bf3f
dave cen <davecen9@gmail.com>
2023-12-24 00:34:57 -0500

    save

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import {createRoot} from 'react-dom/client';
 import {Provider}  from 'react-redux';
 import {store} from "./store";
 import App from "./App";
-
+import "bulma/css/bulma.css";
+import "./styles.css";
 
 const el = document.getElementById('root');
 const root = createRoot(el);
